16c8b92ee399d265d0f1eab3a99009c289b6e410
Breno <breno.asrm@gmail.com>
2017-04-09 19:42:53 -0300

    removing print

--- getMinutesOfMp3Files.py
+++ getMinutesOfMp3Files.py
@@ -6,5 +6,4 @@ for f in files_list:
     if ".mp3" in f:
         audio = MP3(f)
         new_name = str(audio.info.length // 60)[:-2] + 'minutes-' + f
-        print new_name
         os.rename(f,new_name)
